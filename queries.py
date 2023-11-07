bde3e166cab77625038a3db32000ec3c84332e7d
JuhCsabi <c.juhasz@student.utwente.nl>
2023-11-07 17:40:35 +0100

    Added separate python files

---
+++ queries.py
@@ -0,0 +1,83 @@
+import elasticsearch
+import elasticsearch.helpers
+import json
+
+def start_elastic_search():
+    return elasticsearch.Elasticsearch(host='localhost')
+
+
+def read_documents(file_name):
+    """
+    Returns a generator of documents to be indexed by elastic, read from file_name
+    """
+    with open(file_name, 'r') as documents:
+        for line in documents:
+            doc_line = json.loads(line)
+            if ('index' in doc_line):
+                id = doc_line['index']['_id']
+            elif ('PMID' in doc_line):
+                doc_line['_id'] = id
+                yield doc_line
+            else:
+                raise ValueError('Woops, error in index file')
+
+
+def create_index(es, index_name, body={}):
+    # delete index when it already exists
+    es.indices.delete(index=index_name, ignore=[400, 404])
+    # create the index
+    es.indices.create(index=index_name, body=body)
+
+
+def index_documents(es, collection_file_name, index_name, body={}):
+    create_index(es, index_name, body)
+    # bulk index the documents from file_name
+    return elasticsearch.helpers.bulk(
+        es,
+        read_documents(collection_file_name),
+        index=index_name,
+        chunk_size=2000,
+        request_timeout=30
+    )
+
+
+def number_of_indexed_documents(es, index):
+    return es.count(index=index)
+
+
+def get_number_of_results(es, query):
+    search_results = es.search(index='genomics-base', body=query)
+    return search_results['hits']['total']['value']
+
+
+def search_for_str_in_all_fields(es, string, index):
+    query = {
+        "track_total_hits": True,
+        "query": {
+            "query_string": {
+                "query": string,
+                "fields": ["*"]
+            }
+        }
+    }
+    return es.search(index=index, body=query)
+
+
+def search_for_all_str_in_all_fields(es, strings: list, index):
+    query = {
+        "track_total_hits": True,
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "query_string": {
+                            "query": string,
+                            "fields": ["*"]
+                        }
+                    } for string in strings
+                ]
+            }
+        }
+    }
+    return es.search(index=index, body=query)
+
---
+++ trec_files.py
@@ -0,0 +1,63 @@
+from elasticsearch_dsl import Search, Q
+
+def read_qrels_file(qrels_file):  # reads the content of he qrels file
+    trec_relevant = dict()  # query_id -> set([docid1, docid2, ...])
+    with open(qrels_file, 'r') as qrels:
+        for line in qrels:
+            (qid, q0, doc_id, rel) = line.strip().split()
+            if qid not in trec_relevant:
+                trec_relevant[qid] = set()
+            if (rel == "1"):
+                trec_relevant[qid].add(doc_id)
+    return trec_relevant
+
+
+def read_run_file(run_file):
+    # read the content of the run file produced by our IR system
+    # (in the following exercises you will create your own run_files)
+    trec_retrieved = dict()  # query_id -> [docid1, docid2, ...]
+    with open(run_file, 'r') as run:
+        for line in run:
+            (qid, q0, doc_id, rank, score, tag) = line.strip().split()
+            if qid not in trec_retrieved:
+                trec_retrieved[qid] = []
+            trec_retrieved[qid].append(doc_id)
+    return trec_retrieved
+
+
+def read_eval_files(qrels_file, run_file):
+    return read_qrels_file(qrels_file), read_run_file(run_file)
+
+
+def make_trec_run(es, topics_file_name, run_file_name, index_name="genomics", run_name="test"):
+    with open(run_file_name, 'w') as run_file:
+        with open(topics_file_name, 'r') as test_queries:
+            for line in test_queries:
+                (qid, query) = line.strip().split('\t')
+                s = Search(using=es, index=index_name)
+                s = s.extra(size=1000)
+
+                q = Q("multi_match", query=query, fields=['TI', 'AB'])
+
+                response = s.query(q).execute()
+                for i, hit in enumerate(response['hits']['hits']):
+                    run_file.write(f"{qid} Q0 {hit['_source']['PMID']} {i} {hit['_score']} cj_search\n")
+
+
+
+## Probably this one should be used
+def make_trec_run2(es, topics_file_name, run_file_name, index_name="genomics", run_name="test", field="title-abstract"):
+    with open(run_file_name, 'w') as run_file:
+        with open(topics_file_name, 'r') as test_queries:
+            for line in test_queries:
+                (qid, query) = line.strip().split('\t')
+                search_type = "dfs_query_then_fetch"
+                body = {
+                    "query": {
+                        "match" : { field : query }
+                    },
+                    "size": 1000
+                }
+                response = es.search(index=index_name, search_type=search_type, body=body)
+                for i, hit in enumerate(response['hits']['hits']):
+                    run_file.write(f"{qid} {run_name} {hit['_source']['PMID']} {i} {hit['_score']} cj_search2\n")
