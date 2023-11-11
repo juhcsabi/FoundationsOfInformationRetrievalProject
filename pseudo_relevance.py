18cae2479bc025fd31f79626c008ed371fcd8f54
Samuel <s.f.coste@student.utwente.nl>
2023-11-11 12:18:26 +0100

    TFIDF enhanced query

--- pseudo_relevance.py
+++ pseudo_relevance.py
@@ -5,10 +5,14 @@ from nltk.stem import PorterStemmer
 from elasticsearch_dsl import Q
 from elasticsearch_dsl import Search
 import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from operator import itemgetter
 
-# Initialize NLTK components
+# Initialize components
 stop_words = set(stopwords.words('english'))
 english_word_set = set(words.words())
+vectorizer = TfidfVectorizer()
+
 
 
 # Function for text preprocessing
@@ -18,34 +22,79 @@ def preprocess_text(text):
     tokens = [token for token in tokens if token not in string.punctuation]
     tokens = [token for token in tokens if token.lower() not in stop_words]
     tokens = [token for token in tokens if token.lower() in english_word_set]
+    
+    text = ' '.join(tokens)
 
-    return tokens
+    return text
 
 
 
 
-def pseudo_rel(query, retrieved_docs, q_index):
+def get_tfidf(corpus):
+    tfidf_matrix = vectorizer.fit_transform(corpus)
+    
+    terms = vectorizer.get_feature_names_out()
+    
+    tfidf_scores = []
+    
+    for i, doc in enumerate(corpus):
+        # Get the TF-IDF scores for each term in the document
+        doc_tfidf = tfidf_matrix.getrow(i)
+        
+        doc_tfidf_dict = {term: doc_tfidf[0, j] for j, term in enumerate(terms)}
+        
+        sorted_doc_tfidf = sorted(doc_tfidf_dict.items(), key=itemgetter(1), reverse=True)
+        
+        tfidf_scores.append(sorted_doc_tfidf)
+
+    return tfidf_scores
+
+
+    
+    
+def pseudo_rel(query, retrieved_docs, q_index, k_pseudo_docs=10, k_pseudo_terms=10):
+    retrieved_docs_new = []
+    
     if q_index==0:
         print(query, "\n\n")
         
-        for doc in retrieved_docs:
+        corpus=[]
+        
+        for doc in retrieved_docs[:k_pseudo_docs]:
             # Use the Title ('TI') and the Abstract ('AB') to search for words
-            text = doc['TI'] + doc['AB']
+            try:
+                text = doc['TI'] + doc['AB']
             
-            # Return the tokenized text
-            text_terms = preprocess_text(text)
+                text = preprocess_text(text)
 
-            feedback_term_freq = {}
-            for term in text_terms:
-                if term in feedback_term_freq:
-                    feedback_term_freq[term] += 1
-                else:
-                    feedback_term_freq[term] = 1
-
-            # Sort the feedback terms by their frequency
-            sorted_feedback_terms = sorted(feedback_term_freq.items(), key=lambda x: x[1], reverse=True)
-
-            print(sorted_feedback_terms, "\n")
-
+                corpus.append(text)
+            except Exception as e:
+#                 print("EXCEPTION in doc:\n", e, "\n", doc, "\n\n")
+                pass
+        
+        best_terms = get_tfidf(corpus)[0][:k_pseudo_terms]
+        print(best_terms, "\n")
+        
+        new_query = query
+        for new_term in best_terms:
+            new_query += " " + new_term[0]
+        
+        print(new_query, "\n")
+            
     
-    return response
+
+    retrieved_docs_new = retrieved_docs
+    return retrieved_docs_new
+
+
+
+
+
+
+
+
+
+
+
+
+
